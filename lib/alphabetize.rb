318c34a9ba74b8f52e1d6aa727b61ee2473068de
Jeffrey Yu <jwy5140@gmail.com>
2019-03-08 04:42:07 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -3,7 +3,7 @@ require 'pry'
 def alphabetize(arr)
   alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".split("")
   ans = arr.sort_by {|x|
-    alphabet.find_index(x.slice)
+    alphabet.find_index(x.first)
   }
   ans 
 end
